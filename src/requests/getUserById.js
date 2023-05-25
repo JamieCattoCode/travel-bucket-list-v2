af8a556e5df7d7df66705266af34b048824544c6
JamieCattoCode <jamietcatto@gmail.com>
2023-05-25 13:15:36 +0200

    Fixed issue causing unnecessary API calls

--- src/requests/getUserById.js
+++ src/requests/getUserById.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 const getUserById = async (userId) => {
+  if ((!userId) || userId === undefined) {
+    return 'Error: try again with a valid user id';
+  }
   try {
     const { data } = await axios.get(`http://localhost:3001/users/${userId}`);
     const { id, email, username } = data;
